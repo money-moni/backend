28da8027ddcf4b53db927f1590eb815253771fbe
cjh-19 <gns8812@naver.com>
2025-05-06 22:30:53 +0900

    feat: bluetooth 서비스 모듈 추가 (#46, SCRUM-125)

---
+++ ssok-bluetooth-service/src/main/java/kr/ssok/bluetoothservice/SsokBluetoothServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.ssok.bluetoothservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsokBluetoothServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsokBluetoothServiceApplication.class, args);
+    }
+
+}
---
+++ ssok-bluetooth-service/src/test/java/kr/ssok/bluetoothservice/SsokBluetoothServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ssok.bluetoothservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsokBluetoothServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
