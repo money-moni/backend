a317112e03b714a48719271fb64f5ce4fc81fa6c
cjh-19 <gns8812@naver.com>
2025-05-08 14:00:06 +0900

    feat: openfeign error decoder config 적용 (#59, SCRUM-134)

--- ssok-transfer-service/src/main/java/kr/ssok/transferservice/client/OpenBankingClient.java
+++ ssok-transfer-service/src/main/java/kr/ssok/transferservice/client/OpenBankingClient.java
@@ -3,6 +3,7 @@ package kr.ssok.transferservice.client;
 import kr.ssok.common.exception.BaseResponse;
 import kr.ssok.transferservice.client.dto.request.OpenBankingTransferRequestDto;
 import kr.ssok.transferservice.client.dto.response.OpenBankingResponse;
+import kr.ssok.transferservice.config.FeignClientDecoderConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 /**
  * 오픈뱅킹 송금 요청을 보내는 Feign 클라이언트 인터페이스
  */
-@FeignClient(name = "openbanking-service", url = "${external.openbanking-service.url}")
+@FeignClient(name = "openbanking-service", url = "${external.openbanking-service.url}", configuration = FeignClientDecoderConfig.class)
 public interface OpenBankingClient {
 
     /**
