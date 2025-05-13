3f21fd521bbfffbb779585c34d3d9a4d16cda470
isasgp <isasgp@naver.com>
2025-05-13 16:16:23 +0900

    refactor: 에러 응답 코드 수정 (#107, SCRUM-153)

--- ssok-account-service/src/main/java/kr/ssok/accountservice/exception/AccountResponseStatus.java
+++ ssok-account-service/src/main/java/kr/ssok/accountservice/exception/AccountResponseStatus.java
@@ -22,23 +22,22 @@ public enum AccountResponseStatus implements ResponseStatus {
     /**
      * 2. 클라이언트 에러(4000~4999)
      */
-    ACCOUNT_NOT_FOUND(false, 4204, "요청하신 계좌가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    ACCOUNT_ALREADY_EXISTS(false, 4209, "이미 등록된 계좌입니다.", HttpStatus.CONFLICT),
-    ACCOUNT_ALREADY_PRIMARY(false, 4209, "이미 해당 계좌는 주계좌로 설정되어 있습니다.", HttpStatus.CONFLICT),
-    ACCOUNT_PRIMARY_CANNOT_DELETE(false, 4200, "주계좌는 삭제할 수 없습니다.", HttpStatus.BAD_REQUEST),
-    ACCOUNT_CREATION_FORBIDDEN(false, 4203, "본인 명의의 계좌만 연동할 수 있습니다.", HttpStatus.FORBIDDEN),
-    INVALID_BANK_CODE(false, 4200, "유효하지 않은 은행 코드입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_ACCOUNT_TYPE(false, 4200, "유효하지 않은 계좌 유형입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_ACCOUNT_ALIAS(false, 4200, "계좌 별칭이 올바른 형식이 아닙니다.", HttpStatus.BAD_REQUEST),
+    ACCOUNT_NOT_FOUND(false, 4200, "요청하신 계좌가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    ACCOUNT_ALREADY_EXISTS(false, 4201, "이미 등록된 계좌입니다.", HttpStatus.CONFLICT),
+    ACCOUNT_ALREADY_PRIMARY(false, 4202, "이미 해당 계좌는 주계좌로 설정되어 있습니다.", HttpStatus.CONFLICT),
+    ACCOUNT_PRIMARY_CANNOT_DELETE(false, 4203, "주계좌는 삭제할 수 없습니다.", HttpStatus.BAD_REQUEST),
+    ACCOUNT_CREATION_FORBIDDEN(false, 4204, "본인 명의의 계좌만 연동할 수 있습니다.", HttpStatus.FORBIDDEN),
+    INVALID_BANK_CODE(false, 4205, "유효하지 않은 은행 코드입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ACCOUNT_TYPE(false, 4206, "유효하지 않은 계좌 유형입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ACCOUNT_ALIAS(false, 4207, "계좌 별칭이 올바른 형식이 아닙니다.", HttpStatus.BAD_REQUEST),
 
-    OPENBANKING_ACCOUNT_LIST_FAILED(false, 9001, "오픈뱅킹 계좌 목록 조회에 실패했습니다."),
-    OPENBANKING_BALANCE_LOOKUP_FAILED(false, 9002, "오픈뱅킹 계좌 잔액 조회에 실패했습니다."),
-    OPENBANKING_OWNER_LOOKUP_FAILED(false, 9003, "오픈뱅킹 실명 조회에 실패했습니다."),
+    OPENBANKING_ACCOUNT_LIST_FAILED(false, 4208, "오픈뱅킹 계좌 목록 조회에 실패했습니다."),
+    OPENBANKING_BALANCE_LOOKUP_FAILED(false, 4209, "오픈뱅킹 계좌 잔액 조회에 실패했습니다."),
+    OPENBANKING_OWNER_LOOKUP_FAILED(false, 4210, "오픈뱅킹 실명 조회에 실패했습니다."),
+    INVALID_OPENBANKING_CODE(false, 4211, "오픈뱅킹 예외처리 코드가 유효하지 않습니다."),
+    OPENBANKING_REQUEST_FAILED(false, 4212, "오픈뱅킹 API 요청에 실패했습니다."),
 
-    INVALID_OPENBANKING_CODE(false, 9004, "오픈뱅킹 예외처리 코드가 유효하지 않습니다."),
-    OPENBANKING_REQUEST_FAILED(false, 9005, "오픈뱅킹 API 요청에 실패했습니다."),
-
-    USER_INFO_NOT_FOUND(false, 9001, "사용자 정보를 찾을 수 없습니다.");
+    USER_INFO_NOT_FOUND(false, 4213, "사용자 정보를 찾을 수 없습니다.");
 
     private final boolean success;
     private final int code;
