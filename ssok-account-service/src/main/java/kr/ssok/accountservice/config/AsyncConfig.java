9ae5212eec3e5694bc142c5af01c7e0b2143a245
isasgp <isasgp@naver.com>
2025-06-09 10:45:20 +0900

    refactor: 로컬 테스트 완료

--- ssok-account-service/src/main/java/kr/ssok/accountservice/config/AsyncConfig.java
+++ ssok-account-service/src/main/java/kr/ssok/accountservice/config/AsyncConfig.java
@@ -21,7 +21,7 @@ public class AsyncConfig {
         executor.setMaxPoolSize(cores * 20);        // 최대 스레드 수
         executor.setQueueCapacity(500);             // 대기열 용량
         executor.setKeepAliveSeconds(120);          // 임시로 늘어난 스레드 유지 시간
-        executor.setThreadNamePrefix("customWebClient-");   // 스레드 이름 접두어
+        executor.setThreadNamePrefix("customWebClient-");   // 스레드 이름 prefix
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());    // 대기열이 꽉 찬 경우, 서블릿 스레드가 직접 처리
 
         executor.initialize();
